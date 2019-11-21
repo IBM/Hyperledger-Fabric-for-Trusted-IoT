eaf8d39d29188cbecaa6cc2be4165a79d743c175
Yigit Polat <yigitpolatt@gmail.com>
2019-11-21 15:50:28 +0300

    files deleted

--- network-deployment/deployAll.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-kubectl apply -f orderer-deployment.yaml
-kubectl apply -f ca-deployment.yaml
-kubectl apply -f org1peer1-deployment.yaml
-kubectl apply -f org1peer2-deployment.yaml
-kubectl apply -f org2peer1-deployment.yaml
-kubectl apply -f org2peer2-deployment.yaml
