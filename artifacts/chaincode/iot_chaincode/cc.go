b526813ab7b46c25116154c3dab1541cd5a79210
Pratyaksh Gupta <pratyaksh1615@gmail.com>
2021-08-30 17:35:11 +0530

    updating import links for shim and peer package
    The shim and peer protobuf modules are no longer vendored automatically by Fabric, so they are needed to vendor individually.

--- artifacts/chaincode/iot_chaincode/cc.go
+++ artifacts/chaincode/iot_chaincode/cc.go
@@ -7,8 +7,8 @@ import (
 	"strconv"
 	"time"
 
-	"github.com/hyperledger/fabric/core/chaincode/shim"
-	"github.com/hyperledger/fabric/protos/peer"
+	"github.com/hyperledger/fabric-chaincode-go/shim"
+	sc "github.com/hyperledger/fabric-protos-go/peer"
 )
 
 //SmartContract
